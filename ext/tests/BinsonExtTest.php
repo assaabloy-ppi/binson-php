1f648020b739334642c19899c0c699ab8ba28502
ppmag <mybestexpert@gmail.com>
2018-08-23 16:25:54 +0300

    ext basic writer tests, in progress

--- ext/tests/BinsonExtTest.php
+++ ext/tests/BinsonExtTest.php
@@ -6,27 +6,65 @@ require_once(__DIR__ . '/../out/binson.php');
 class BinsonExtTest extends TestCase
 {
     public function testBinsonExtensionIsAvailable()
-    {
-        $writer = new binson_writer();        
-        $parser = new binson_parser();
+    {   
+        $buf = "_______";
+        $writer = new BinsonWriter($buf);
+        //$parser = new binson_parser();
 
-        $this->assertInstanceOf(binson_writer::class, $writer);
-        $this->assertInstanceOf(binson_parser::class, $parser);
+        $this->assertInstanceOf(BinsonWriter::class, $writer);
+        //$this->assertInstanceOf(binson_parser::class, $parser);
     }
 
-    public function testEmptyBinsonObject()
+    public function testEmptyBinsonObject() 
     {
-        $writer = new binson_writer();        
-        $buf =  str_repeat( '0', 1024 );
+        $buf = "____";
+        $writer = new BinsonWriter($buf);
 
-        //binson::binson_writer_init( $writer, (SWIGTYPE_p_uint8_t)$buf, 1024 );
-        //binson::binson_writer_object_begin( $writer );
-        //binson::binson_writer_object_end( $writer );
+        $writer->objectBegin()
+               ->objectEnd();
 
-        //$this->assertTrue($buf[0] == 0x40);
-        //$this->assertTrue($buf[1] == 0x41);
-
+        $this->assertEquals($buf, "\x40\x41__");
+        $this->assertEquals($writer->length(), 2);
+        $this->assertEquals($writer->toString(), "\x40\x41");
     }
+
+    public function testEmptyBinsonArray() 
+    {
+        $buf = "____";
+        $writer = new BinsonWriter($buf);
+
+        $writer->arrayBegin()
+               ->arrayEnd();
+
+        $this->assertEquals($buf, "\x42\x43__");
+        $this->assertEquals($writer->length(), 2);
+        $this->assertEquals($writer->toString(), "\x42\x43");
+    }
+
+    public function testBooleans() 
+    {
+        $buf = "____";
+        $writer = new BinsonWriter($buf);
+
+        $writer->boolean(true)
+               ->boolean(false)
+               ->trueValue()
+               ->falseValue();
+
+        $this->assertEquals($writer->toString(), "\x44\x45\x44\x45");
+    }
+
+    public function testIntegers() 
+    {
+        $buf = str_repeat('_', 128);
+        $writer = new BinsonWriter($buf);
+
+        $writer->integer(2147483647);
+
+        $this->assertEquals($writer->toString(), "\x12\xff\xff\xff\x7f");
+    }
+
+
 }
 
 ?>
